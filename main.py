145c5e1457f3d9faf409275c1f5b29f3226fffe5
vipuldevre123 <73344771+vipuldevre123@users.noreply.github.com>
2022-04-19 17:38:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics
+import random
+import pandas as pd
+import csv
+
+df = pd.read_csv("studentMarks.csv")
+data = df["Math_score"].tolist()
+
+#plotting the graph
+fig = ff.create_distplot([data],["Math Scores"], show_hist= False)
+fig.show()
+
+mean=statistics.mean(data)
+sd=statistics.stdev(data)
+
+print(mean)
+print(sd)
